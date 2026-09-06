74a4e87c82cf16b6d86a3ac591d09fb45bc5467c
mainbotan <ned.serafim@gmail.com>
2026-09-06 09:02:25 +0300

    cars-block

--- src/app/(external)/(main)/page.tsx
+++ src/app/(external)/(main)/page.tsx
@@ -2,11 +2,13 @@ import clsx from 'clsx';
 import StartSlide from './(slides)/start-slide/slide';
 import styles from './page.module.scss';
 import { Slide } from '../components/slide/slide';
+import CarsSlide from './(slides)/cars-slide/slide';
 
 export default function MainPage() {
     return (
         <>
         <Slide className={clsx(styles.slide)}><StartSlide className={styles.slideContent} /></Slide>
+        <Slide className={clsx(styles.slide)}><CarsSlide className={styles.slideContent} /></Slide>
         </>
     )
 }
